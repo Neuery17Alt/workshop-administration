f4d9f610230c997710513ad3565a9f1e26c7de23
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-09-20 15:02:28 +0200

    change: created pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            title: "Workshop Administration",
-        },
-    },
-    compatibilityDate: '2024-04-03',
-    devtools: {enabled: true}
+  app: {
+      head: {
+          title: "Workshop Administration",
+      },
+  },
+
+  compatibilityDate: '2024-04-03',
+  devtools: {enabled: true},
+  modules: [
+    "@pinia/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss"
+  ]
 })
