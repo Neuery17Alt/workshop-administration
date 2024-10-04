b0b12fb7f78082dcb211276d5f85ed1a79e090cc
Neuer17Alt <jangerer@htl-steyr.ac.at>
2024-10-04 08:56:35 +0200

    update: fixed single workshop fetch, delete workshop not working

--- server/api/workshop/[id].get.ts
+++ server/api/workshop/[id].get.ts
@@ -1,23 +1,27 @@
 import { serverSupabaseClient } from "#supabase/server"
 import type { Database } from "@/types/supabase"
-import {Teacher} from "~/types/workshop.types";
+import {Teacher, Workshop} from "~/types/workshop.types";
 
 export default defineEventHandler(async (event) => {
     const uuid = getRouterParam(event, "id")
     const supabase = await serverSupabaseClient<Database>(event)
 
-    const { data: teacher } = await supabase
-        .from("Teacher")
-        .select(
-            "*",
-        )
+    const { data: workshop } = await supabase
+        .from("Workshop")
+        .select(`
+        *, 
+        Student(*),
+        Category(*), 
+        Teacher(*),
+        Subject(*)
+        `)
         .eq("uuid", `${uuid}`)
         .single()
-        .returns<Teacher>()
+        .returns<Workshop>()
 
-    if (!teacher) {
+    if (!workshop) {
         return []
     }
 
-    return teacher
+    return workshop
 })
